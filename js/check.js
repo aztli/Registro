4a63d779b00c6041eafb9679798b86fed321536e
Miguel Angel Gordian <os.aioria@gmail.com>
2012-12-08 02:50:42 -0600

    Elementos basicos de la pagina.
    Creacion de la pagina a travez de elementos ya definidos por medio
    de la hoja de estilo (css), de bootstrap.
    
    La validacion y limpieza se realiza a travez de el elemento
    `check.js`.
    
    EL proyecto esta en fase alpha, faltan por agregar mas detalle en
    cuanto a los errores de campos fallidos, ademas falta la pagina de
    respuesta.

---
+++ js/check.js
@@ -0,0 +1,149 @@
+window.onload = function () {
+    // Validacion de campos de entrada
+    document.getElementById("inputname").onchange = usercheck;
+    document.getElementById("inputlastname").onchange = lastnamecheck;
+    document.getElementById("inputemail").onchange = emailcheck;
+    // document.getElementById("woman").onclick = sexcheck
+    // document.getElementById("men").onclick = sexcheck
+    document.getElementById("inputuniv").onchange = univcheck;
+    document.getElementById("inputdegree").onchange = degreecheck;
+
+    // asignacion de acciones a botones
+    document.getElementById("reset").onclick = limpiatodo;
+    document.getElementById("send").onclick = enviarform;
+}
+
+
+function usercheck(){
+    // Validacion de usuario
+    var user = document.getElementById("inputname")
+    var userbool = user.value.length > 2;
+
+    // console.log(userbool) // Comenzamos con el habito de debuguear con
+                             // console.log()
+
+    color_status(userbool,
+		 user.parentNode.parentNode);
+
+    return userbool;
+}
+
+function lastnamecheck(){
+    // Validacion de apellido
+    var lastname = document.getElementById("inputlastname")
+    var lastnamebool = lastname.value.length > 2
+
+    color_status(lastnamebool,
+		 lastname.parentNode.parentNode);
+
+    return (lastname.value.length > 2);
+}
+
+function emailcheck() {
+    // Valicion de email - regexp basico
+    var remail = /^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
+    var email = document.getElementById("inputemail");
+    var emailbool = remail.test(email.value);
+
+    color_status(emailbool,
+		 email.parentNode.parentNode);
+
+    return emailbool;
+}
+
+function sexcheck() {
+    // Primero las damas
+    var woman = document.getElementById("woman");
+    var men = document.getElementById("men");
+
+
+    color_status((woman.checked || men.checked),
+		 woman.parentNode.parentNode.parentNode);
+
+    if (woman.checked){
+	console.log("Le gustaria ir a cenar esta noche?");
+	return woman.value;
+    }
+    else if (men.checked){
+	console.log("Tomamos una cerveza y luego unas vencidas?");
+	return men.value;
+    }
+    else{
+	console.log("¡Cielos, no estaba preparado para esto!")
+	return false;
+    }
+}
+
+
+function univcheck(){
+    // validacion de universidad
+    var univ = document.getElementById("inputuniv");
+    var univbool = univ.value.length > 2;
+
+    color_status(univbool,
+		 univ.parentNode.parentNode);
+
+    return (univ.value.length > 2);
+}
+
+function degreecheck(){
+    // validacion de carrera
+    var degree = document.getElementById("inputdegree");
+    var degreebool = degree.value.length > 6;
+    color_status(degreebool,
+		 degree.parentNode.parentNode);
+
+    return degreebool;
+}
+
+
+function limpiatodo(){
+    /* 
+     * Pero la mayoria de los que pudieron hacer algo por impedir el crimen y
+     * sin embargo no lo hicieron, se consolaron con el pretexto de que los
+     * asuntos de honor son estancos sagrados a los cuales sólo tiene acceso
+     * los dueños del drama.
+     */
+
+    items = ["inputname","inputlastname","inputemail","inputuniv","inputdegree"]
+
+    for (item=0; item < items.length; item++) {
+	tmpelement = document.getElementById(items[item]);
+	tmpelement.value = "";
+	color_status("reset", tmpelement.parentNode.parentNode);
+    }
+
+    tmpelement = "";
+
+    document.getElementById("woman").checked = true;
+}
+
+function color_status(value, objectclass)
+{
+    if (value == "reset"){
+	objectclass.className = "control-group";
+    }
+    else if (value){
+	objectclass.className = "control-group success";
+    }
+    else {
+	objectclass.className = "control-group error";
+    }
+}
+
+function enviarform(){
+    /*
+     * Al día siguiente tuvo que mandar un telegrama, y él mismo lo 
+     * transmitió con el manipulador y además le enseño al telegrafista
+     * una fórmula suya para seguir usando las pilas agotadas.
+     */
+    if (usercheck() && lastnamecheck() && emailcheck() &&
+	univcheck() &&  degreecheck()) {
+	sexcheck(); // Como se declara??
+	document.register.submit();
+    }
+    else{
+	console.log("¡Algo a sucedido mal capitan!")
+	return 0;
+    }
+}
